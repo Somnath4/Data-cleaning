15ee3ed281ad0cf45545159717b29e3b8395eb63
Somnath Malik <114238887+Somnath4@users.noreply.github.com>
2022-12-07 19:34:10 +0530

    Update DS_job queries.sql

--- DS_job queries.sql
+++ DS_job queries.sql
@@ -1,3 +1,4 @@
+-- Let's check the data set
 SELECT *
 FROM ds_job
 LIMIT 10;
@@ -7,20 +8,20 @@ SELECT Salary_Estimate,
        SUBSTRING_INDEX(Salary_Estimate,' ', 1) salary_estimate
 FROM ds_job;
 
--- update the table with new salary estimate
+-- Updating the table with new salary estimate
 UPDATE ds_job
 SET Salary_Estimate = SUBSTRING_INDEX(Salary_Estimate,' ', 1);
 
 SELECT *
 FROM ds_job;
 
--- removing rating from the `Company_Name`
+-- Removing rating from the `Company_Name`
 SELECT `Company_Name`,
        SUBSTRING_INDEX(`Company_Name`, '
 ', 1) company_name       
 FROM ds_job;
 
---counting and comparing charactor length 
+-- Counting charactor length  and comparing with the the Comapny_name column 
 WITH subtring AS (SELECT `Company_Name`,
        SUBSTRING_INDEX(`Company_Name`, '
 ', 1) company       
@@ -33,7 +34,7 @@ FROM ds_job D
 JOIN subtring S
 ON D.`Company_Name` = S.`Company_Name`;
 
---update the column Company_Name
+-- Updating the column Company_Name
 UPDATE ds_job 
 SET `Company_Name` = SUBSTRING_INDEX(`Company_Name`, '
 ', 1);
@@ -41,7 +42,7 @@ SET `Company_Name` = SUBSTRING_INDEX(`Company_Name`, '
 SELECT *
 FROM ds_job;
 
--- removing company from `Type_of_ownership`
+-- Removing the 'Company' in `Type_of_ownership`
 SELECT `Type_of_ownership`,
        SUBSTRING_INDEX(`Type_of_ownership`, '- ', -1) ownership
 FROM ds_job;
@@ -49,13 +50,11 @@ FROM ds_job;
 UPDATE ds_job
 SET `Type_of_ownership` = SUBSTRING_INDEX(`Type_of_ownership`, '- ', -1);
 
---replacing value -1 with unknown.
-
+-- See the new table
 SELECT *
 FROM ds_job;
 
-
---counting each column where value is -1
+-- Counting '-1' in each column these columns
 SELECT (SELECT COUNT(Rating) FROM ds_job WHERE Rating = -1)  rating, 
        (SELECT COUNT(`Headquarters`) FROM ds_job WHERE `Headquarters`= -1) headquarters, 
        (SELECT COUNT(Size) FROM ds_job WHERE `Size` = -1) size,
@@ -66,7 +65,7 @@ SELECT (SELECT COUNT(Rating) FROM ds_job WHERE Rating = -1)  rating,
 FROM ds_job
 LIMIT 1;
 
--- now remove the -1 values with unknown and Unknown / Non-Applicable with Unknown in Revenue column
+-- Now, removing the '-1' values with 'Unknown' and 'Unknown / Non-Applicable' with Unknown in Revenue column
 UPDATE ds_job
 SET `Rating` = REPLACE(Rating, '-1', '0')
 WHERE `Rating` = -1;
@@ -89,10 +88,15 @@ UPDATE ds_job
 SET `Revenue` = REPLACE(`Revenue`, '-1', 'Unknown')
 WHERE `Revenue` = '-1';
 UPDATE ds_job
+SET Revenue = REPLACE(`Revenue`, 'Unknown / Non-Applicable', 'Uknown')
+WHERE `Revenue` = 'Unknown / Non-Applicable';
+
+-- There was a spelling mistake while changing the Revenue column value 'Unknown / Non-Applicable' to 'Unkown'
+-- Now, we're going to change 'Uknown' to 'Unknown'  
 SET Revenue = REPLACE(`Revenue`, 'Uknown', 'Unknown')
 WHERE `Revenue` = 'Uknown';
 
--- deleting unuse columns Index_, Founded and competitors
+-- Deleting unuse columns Index_, Founded and Competitors
 ALTER TABLE ds_job
 DROP COLUMN index_,
 DROP COLUMN Founded,
